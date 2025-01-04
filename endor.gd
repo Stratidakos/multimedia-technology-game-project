e70e8ff3b80c4134a1410c02609726e8c3e927e5
g.stratidis <g.stratidis@clmsuk.com>
2025-01-05 00:46:10 +0200

    Progress on 1st level

--- endor.gd
+++ endor.gd
@@ -1,5 +1,6 @@
-extends Node2D
+extends ParallaxBackground
 
+var scrollingSpeed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _on_geonosis_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://1_endor.tscn")
+	scroll_offset.x -= scrollingSpeed * delta
